43c1169332b6adf0faa9aaf60d81af0e34f0e941
DongJin Kim <nyong9221@gmail.com>
2022-10-21 14:26:25 +0900

    initialize

---
+++ src/main/java/TodoApplication/TodoApplication/Todo.java
@@ -0,0 +1,15 @@
+package TodoApplication.TodoApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Todo {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application !";
+
+
+    }
+}
